d5e5257dab475eae2d1d58b6573c6030127995fa
Marlon Marin <marlonmariin97@outlook.com>
2023-08-19 18:43:55 -0500

    Advances in a function for moving interesting files to their corresponding directory.

--- interact_os/example_folder/organizer.py
+++ interact_os/example_folder/organizer.py
@@ -12,15 +12,16 @@ def check_ext(ext):
 def create_directory(ext_name):
     new_dir = f'{ext_name.replace(".", "")}'
     os.makedirs(f'./{new_dir}', exist_ok=True)
-    
-def extract_ext():
+    return f'./{new_dir}'
+
+def extract_ext(path):
     empty = []
-    for k in os.listdir():
+    for k in os.listdir(path):
         empty.append(check_ext(k))
     return empty
 
 def search_matches(from_cwd, list_of_extensions):
-    matches = []
+    matches = {}
     for item in from_cwd:
         ext_not_found = True
         for key, value in list_of_extensions.items():
@@ -28,7 +29,8 @@ def search_matches(from_cwd, list_of_extensions):
                 ext_not_found=False
                 break
         if ext_not_found:
-            matches.append((item, 'Not a category'))
+            a, b = (item, 'Miscellaneous')
+            matches[a] = b
             
     for item in from_cwd:
         ext_found = True
@@ -37,16 +39,28 @@ def search_matches(from_cwd, list_of_extensions):
                 ext_found=False
                 break
         if not ext_found:
-            matches.append((item, key))
+            a,b = (item, key)
+            matches[a] = b
     return matches
+
+def chdir(path, ext_dict):
+    for filename in os.listdir(path):
+        for ext, dir in ext_dict.items():
+            if filename.lower().endswith(ext):
+                new_dir = create_directory(dir)
+                os.rename(filename, new_dir)
+                
+        
 def run():
     extensions = {'Multimedia' : ['.mp3', '.mp4', '.png', '.bmp'], 
                   'Office': ['.docx', '.xslx', '.ppt', '.txt', '.csv'],
                   'Code': ['.py', '.js', '.php']
                   }
 
-    from_cwd = set(extract_ext())
-    print(search_matches(from_cwd, extensions))
+    ext_list_in_cwd = set(extract_ext('./'))
+    searched_matches = search_matches(ext_list_in_cwd, extensions)
+    chdir('./', searched_matches)
+    # print(list(searched_matches.values()))
 
 if __name__ == '__main__':
     run()
