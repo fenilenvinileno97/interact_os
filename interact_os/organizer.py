938d29671ef52fb320896db4ea9d9ef45017f5ae
Marlon Marin <marlonmariin97@outlook.com>
2023-08-20 17:04:58 -0500

    organizer.py works, path variable is the working directory for organize files into their respective categories. Categories are stored in the extensions dictionary, import from another file could be a good idea for a more solid code. Files in the example_folder/ are now classified.

--- interact_os/organizer.py
+++ interact_os/organizer.py
@@ -11,12 +11,14 @@ def return_extension(path):
     return set(re.findall(r"\.\w+", result.stdout.decode()))
  
 #this function creates directories based on the file category    
-def create_directory(category):
+def create_directory(path, category):
     new_dir = f'{category.replace(".", "")}'
-    os.makedirs(f'./{new_dir}', exist_ok=True)
+    new_dir_path = os.path.join(path, new_dir)
+    os.makedirs(new_dir_path, exist_ok=True)
     return f'./{new_dir}'
 
-def search_matches(from_cwd, list_of_extensions):
+#this function matches files found in return_extension with a known file category 
+def set_dir_matches(from_cwd, list_of_extensions):
     matches = {}
     for item in from_cwd:
         ext_not_found = True
@@ -39,19 +41,27 @@ def search_matches(from_cwd, list_of_extensions):
             matches[a] = b
     return matches
 
+#this function moves files into recently created directories
 def chdir(path, ext_dict):
+    path = os.path.abspath(path)
     for filename in os.listdir(path):
         for ext, dir in ext_dict.items():
             if filename.lower().endswith(ext):
-                print(filename, dir)
-                
-        
+                create_directory(path, dir)
+                current_path = os.path.join(path, filename)
+                new_path = os.path.join(os.path.dirname(current_path), dir+'/'+filename)
+                os.rename(current_path, new_path)
+               
 def run():
     extensions = {'Multimedia' : ['.mp3', '.mp4', '.png', '.bmp'], 
                   'Office': ['.docx', '.xslx', '.ppt', '.txt', '.csv'],
                   'Code': ['.py', '.js', '.php']
                   }
-    print(return_extension('./example_folder'))
-    print(return_extension('./'))
+    pathway = './example_folder'
+    from_cwd = return_extension(pathway)
+    found_matches = set_dir_matches(from_cwd, extensions)
+    chdir(pathway, found_matches)
+
+    
 if __name__ == '__main__':
     run()
